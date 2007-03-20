8e5c091350d31a81d0090e59cd8dd38d89e1ad74
violent_ken <violent_ken@6c7c5f9e-f928-0410-8fd2-9fea538ed778>
2007-03-20 15:51:43 +0000

    Fichier/répertoire effacé

--- Executable folder/UnRegisterOCX.bat
+++
@@ -1,6 +0,0 @@
-regsvr32 BGraphe_OCX.ocx -s -u
-regsvr32 ExtendedVScrollbar_OCX.ocx -s -u
-regsvr32 FileView_OCX.ocx -s -u
-regsvr32 ProgressBar_OCX.ocx -s -u
-regsvr32 HexViewer_OCX.ocx -s -u
-regsvr32 LanguageTranslator.ocx -s -u
