18fdc4704527cf7a4c2c87825329f321ea21b6cc
violent_ken <alaindescotes@gmail.com>
2007-04-03 17:33:36 +0000

    Fixed Projects bad references + added 2 *.ocx to *.bat files

--- Executable folder/UnRegister files.bat
+++ Executable folder/UnRegister files.bat
@@ -5,3 +5,5 @@ regsvr32 ProgressBar_OCX.ocx -s -u
 regsvr32 HexViewer_OCX.ocx -s -u
 regsvr32 LanguageTranslator.ocx -s -u
 regsvr32 Disassembler_DLL.dll -s -u
+regsvr32 ProcessView_OCX.ocx -s -u
+regsvr32 DriveView_OCX.ocx -s -u
