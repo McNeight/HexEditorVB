7047977bcd9aaaa9c8e0b9af1fbf508b2adeb2f3
violent_ken <alaindescotes@gmail.com>
2007-04-30 17:49:37 +0000

    Deleted LanguageTranslator.ocx

--- Executable folder/Register files.bat
+++ Executable folder/Register files.bat
@@ -3,7 +3,6 @@ regsvr32 ExtendedVScrollbar_OCX.ocx -s
 regsvr32 FileView_OCX.ocx -s
 regsvr32 ProgressBar_OCX.ocx -s
 regsvr32 HexViewer_OCX.ocx -s
-regsvr32 LanguageTranslator.ocx -s
 regsvr32 Disassembler_DLL.dll -s
 regsvr32 ProcessView_OCX.ocx -s
 regsvr32 DriveView_OCX.ocx -s
--- Executable folder/UnRegister files.bat
+++ Executable folder/UnRegister files.bat
@@ -3,7 +3,6 @@ regsvr32 ExtendedVScrollbar_OCX.ocx -s -u
 regsvr32 FileView_OCX.ocx -s -u
 regsvr32 ProgressBar_OCX.ocx -s -u
 regsvr32 HexViewer_OCX.ocx -s -u
-regsvr32 LanguageTranslator.ocx -s -u
 regsvr32 Disassembler_DLL.dll -s -u
 regsvr32 ProcessView_OCX.ocx -s -u
 regsvr32 DriveView_OCX.ocx -s -u
